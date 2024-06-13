69ebec8835b7b1a6c841e92634f50024211ef52e
hyonejin <rhkddlr98@naver.com>
2024-06-13 14:30:34 +0900

    스프링부트 환경설정 완료

---
+++ CowStallManagement/src/main/java/miniPrj/CowStallManagement/CowStallManagementApplication.java
@@ -0,0 +1,13 @@
+package miniPrj.CowStallManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CowStallManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CowStallManagementApplication.class, args);
+	}
+
+}
---
+++ CowStallManagement/src/test/java/miniPrj/CowStallManagement/CowStallManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package miniPrj.CowStallManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CowStallManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
